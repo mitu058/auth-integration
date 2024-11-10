790efae2387e37d613230b6b2b353c8948784028
mitu058 <mitum9796@gmail.com>
2024-11-11 00:10:57 +0600

    creat login and signup from

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const handelSignUp = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+      };
     return (
-        <div>
-            <h2>sisgnup Now</h2>
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-4xl font-bold">SignUp now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handelSignUp} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+               
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">SignUp</button>
+              </div>
+            </form>
+            <p className="p-4">
+              Already have an account? please <Link to="/login"><u>LogIn</u></Link>
+            </p>
+          </div>
         </div>
+      </div>
     );
 };
 
